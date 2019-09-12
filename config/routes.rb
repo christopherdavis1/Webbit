a2a4ee03faee588e75b2f25401b478c059dc16e0
Christopher Davis <chris@christopheralandavis.com>
2019-09-11 23:35:20 -0400

    Added submissions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :models
+  resources :submissions
   devise_for :users
   root to: "home#index"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_010725) do
+ActiveRecord::Schema.define(version: 2019_09_12_030916) do
+
+  create_table "submissions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
